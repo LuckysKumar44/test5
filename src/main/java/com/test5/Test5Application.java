fba0c0c9461c82caf39ab27bc5a9e7eafe03d4c5
LuckysKumar44 <luckyskumarm@gmail.com>
2025-03-25 04:03:25 +0530

    Update Test5Application.java

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test5Application {
 
 	public static void main(String[] args) {
+		String name = "mike";
+		int x = 100;
+		int y = 200;
+		double z = 10.3;
 		SpringApplication.run(Test5Application.class, args);
 	}
 
